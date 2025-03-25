ef393d84cd321d00e874fcce14340816cc2a3225
Henrik Norlin <henrik.norl@gmail.com>
2025-03-25 06:14:42 +0100

    project_sequence_multi: double-quotes

--- project_sequence_multi/models/res_config_settings.py
+++ project_sequence_multi/models/res_config_settings.py
@@ -8,15 +8,15 @@ _logger = logging.getLogger(__name__)
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     project_sequence_multi_field = fields.Many2one(
         string="Project Sequence Field",
         comodel_name="ir.model.fields",
         domain="""[
-            ('model_id', '=', 'project.project'),
-            ('required', '=', True),
-            ('ttype', 'in', ['boolean', 'selection']),
+            ("model_id", "=", "project.project"),
+            ("required", "=", True),
+            ("ttype", "in", ["boolean", "selection"]),
         ]""",
     )
 
@@ -25,21 +25,21 @@ class ResConfigSettings(models.TransientModel):
         res = super(ResConfigSettings, self).get_values()
         field = self._get_project_sequence_field()
         if field:
-            res['project_sequence_multi_field'] = field.id
+            res["project_sequence_multi_field"] = field.id
         else:
-            res['project_sequence_multi_field'] = False
+            res["project_sequence_multi_field"] = False
         return res
     
     def _get_project_sequence_field(self):
         # model_name.field_name -> field.id
         field = False
-        model_field = self.env['ir.config_parameter'].sudo().get_param('project_sequence_multi_field', default=False)
+        model_field = self.env["ir.config_parameter"].sudo().get_param("project_sequence_multi_field", default=False)
         if model_field:
-            model_name, field_name = model_field.rsplit('.', 1)
-            field = self.env['ir.model.fields'].search(
+            model_name, field_name = model_field.rsplit(".", 1)
+            field = self.env["ir.model.fields"].search(
                 [
-                    ('model_id.model', '=', model_name),
-                    ('name', '=', field_name),
+                    ("model_id.model", "=", model_name),
+                    ("name", "=", field_name),
                 ],
             ).ensure_one()
         return field
@@ -53,26 +53,26 @@ class ResConfigSettings(models.TransientModel):
             model_field = f"{field.model}.{field.name}"
         else:
             model_field = ""
-        Parameter = self.env['ir.config_parameter']
-        Parameter.sudo().set_param('project_sequence_multi_field', model_field)
+        Parameter = self.env["ir.config_parameter"]
+        Parameter.sudo().set_param("project_sequence_multi_field", model_field)
         self._create_project_sequences(model_field)
 
     def _create_project_sequences(self, model_field):
         Sequence = self.env["ir.sequence"]
         field = self.project_sequence_multi_field
         if field:
-            if field.ttype == 'boolean':
+            if field.ttype == "boolean":
                 seq_field_values = ["yes", "no"]
-            elif field.ttype == 'selection':
+            elif field.ttype == "selection":
                 seq_field_values = field.selection_ids.mapped("value")
             for value in seq_field_values:
                 code = f"{model_field}.{value}"
-                seq = Sequence.search([('code', '=', code)])
+                seq = Sequence.search([("code", "=", code)])
                 if not seq:
                     seq = Sequence.create({
-                        'name': code,
-                        'code': code,
-                        'padding': 5,
-                        'prefix': value + "-",
+                        "name": code,
+                        "code": code,
+                        "padding": 5,
+                        "prefix": value + "-",
                     })
                     _logger.info(f"Created sequence {seq.name}")
