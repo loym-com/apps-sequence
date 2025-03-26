5c176991c089d29c77aa9597a1cf1721cfffcdc0
Henrik Norlin <henrik.norl@gmail.com>
2025-03-26 15:28:28 +0100

    [FIX] display_name (cleanup)

--- display_name/models/base.py
+++ display_name/models/base.py
@@ -1,11 +1,8 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
-
 import re
 
-from odoo import api, fields, models
-from odoo.exceptions import UserError
+from odoo import api, models
+from odoo.osv import expression
 from odoo.tools.translate import _
-
 
 class Base(models.AbstractModel):
     _inherit = "base"
@@ -25,7 +22,6 @@ class Base(models.AbstractModel):
             self._get_display_name_re(), self._get_display_name_pattern()
         )
         return tuple(fields)
-
     
     @api.depends(lambda self: self._get_display_name_fields())
     def _compute_display_name(self):
@@ -47,14 +43,14 @@ class Base(models.AbstractModel):
 
             return True
 
-        reg_exp = self._get_display_name_re()
         pattern = self._get_display_name_pattern()
+        field_names = self._get_display_name_fields()
         for record in self:
             # Dynamically fetch field values based on the pattern
             try:
                 field_values = {
                     field_name: getattr(record, field_name, "")
-                    for field_name in record._get_display_name_fields()
+                    for field_name in field_names
                 }
                 # Condition for display_name
                 if fields_are_filled_and_different(field_values):
@@ -64,55 +60,16 @@ class Base(models.AbstractModel):
                     f"Field '{e.args[0]}' does not exist in model '{self._name}'."
                 )
 
-    # @staticmethod
-    # def _extract_field_names(pattern):
+    @api.model
+    def _search_display_name(self, operator, value):
+        search_fnames = self._get_display_name_fields()
+        if not search_fnames:
+            return super()._search_display_name(operator, value)
 
-    # @staticmethod
-    # def _get_display_name_pattern_field_names(self):
-    #     pattern = self._get_display_name_pattern()
-    #     # Example: "%(sequence_code)s - %(name)s" -> ["sequence_code", "name"]
-    #     return re.findall(r"%\((\w+)\)s", pattern)
-
-
-    # @api.model
-    # def name_search(self, name="", args=None, operator="ilike", limit=100):
-    #     """Allow searching by sequence code by default."""
-    #     result = super().name_search(name, args, operator, limit)
-    #     # Do not add any domain when user just clicked on search widget
-    #     if not (name == "" and operator == "ilike"):
-    #         # We need additional search, as in super() method call expression
-    #         # `AND` is used and there is no easy way to add `OR` in the final domain
-    #         projects = self.search_fetch(
-    #             [
-    #                 ("sequence_code", operator, name),
-    #                 "!",
-    #                 ("display_name", operator, name),
-    #             ],
-    #             ["display_name"],
-    #             limit=limit,
-    #         ).mapped(lambda p: (p.id, p.display_name))
-    #         result.extend(projects)
-    #     return result
-
-    # def name_search(self, name="", args=None, operator="ilike", limit=100):
-    #     """
-    #     Override name_search to search across all fields in the display_name_pattern.
-    #     """
-    #     result = super().name_search(name, args, operator, limit)
-
-    #     args = args or []
-    #     domain = args
-
-    #     if name:
-    #         # Extract fields from the display_name_pattern
-    #         field_names = self._get_display_name_fields()
-    #         search_domains = [
-    #             (field_name, operator, name) for field_name in field_names if field_name in self._fields
-    #         ]
-    #         domain = ["|"] * (len(search_domains) - 1) + search_domains + args
-
-    #     return self.search(domain, limit=limit).name_get()
-    
-    #     # normal search
-    #     result.extend(super().name_search(name, domain, operator, limit))
-    #     return result
+        # Copied from ORM models.py
+        if operator.endswith('like') and not value and '=' not in operator:
+            # optimize out the default criterion of ``like ''`` that matches everything
+            # return all when operator is positive
+            return expression.FALSE_DOMAIN if operator in expression.NEGATIVE_TERM_OPERATORS else expression.TRUE_DOMAIN
+        aggregator = expression.AND if operator in expression.NEGATIVE_TERM_OPERATORS else expression.OR
+        return aggregator([[(field_name, operator, value)] for field_name in search_fnames])
