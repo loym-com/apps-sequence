62a471c5e19c13a3790467ebf9644576c83e206f
Henrik Norlin <henrik.norl@gmail.com>
2025-04-07 11:17:54 +0200

    18.0.1.0.4

--- sequence/__manifest__.py
+++ sequence/__manifest__.py
@@ -13,6 +13,6 @@
         "base_setup",
     ],
     "license": "LGPL-3",
-    "version": "18.0.1.0.3",
+    "version": "18.0.1.0.4",
     "website": "https://www.loym.com",
 }
--- sequence_choice/__manifest__.py
+++ sequence_choice/__manifest__.py
@@ -10,6 +10,6 @@
     ],
     "depends": ["sequence"],
     "license": "AGPL-3",
-    "version": "18.0.1.0.3",
+    "version": "18.0.1.0.4",
     "website": "https://www.loym.com",
 }
