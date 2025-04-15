376f74c0c3cdb9af07dcd8ed3e92ad4a6938e6ba
Henrik Norlin <henrik.norl@gmail.com>
2025-04-16 00:20:10 +0200

    18.0.1.0.12 requirements.txt

--- sequence/__manifest__.py
+++ sequence/__manifest__.py
@@ -14,6 +14,6 @@
         "display_name", # @api.ondelete(at_uninstall=True) in base.py
     ],
     "license": "LGPL-3",
-    "version": "18.0.1.0.11",
+    "version": "18.0.1.0.12",
     "website": "https://www.loym.com",
 }
