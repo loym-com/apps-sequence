b5caf4da74d2d111b619c1fe374dea914ea109b0
Henrik Norlin <henrik.norl@gmail.com>
2025-04-21 17:27:39 +0200

    18.0.2.0.5 deleted inactive product.product.view.form

--- crm_unique_code/__manifest__.py
+++ crm_unique_code/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "CRM Unique Code",
-    "version": "18.0.2.0.4",
+    "version": "18.0.2.0.5",
     "author": "Loym",
     "website": "https://www.loym.com",
     "license": "LGPL-3",
--- project_unique_code/__manifest__.py
+++ project_unique_code/__manifest__.py
@@ -15,6 +15,6 @@
         "project",
     ],
     "license": "LGPL-3",
-    "version": "18.0.2.0.4",
+    "version": "18.0.2.0.5",
     "website": "https://www.loym.com",
 }
