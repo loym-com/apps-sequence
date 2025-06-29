b8f8e9b9bf02fd8f9199a33218f0ac57e752754e
Henrik Norlin <henrik.norl@gmail.com>
2025-06-29 23:16:14 +0200

    18.0.3.0.18 main variant with domain

--- product_unique_code/__manifest__.py
+++ product_unique_code/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Product Unique Code",
     "summary": " ",
-    "version": "18.0.3.0.17",
+    "version": "18.0.3.0.18",
     "author": "Cetmix, Loym",
     "website": "https://www.loym.com",
     "license": "LGPL-3",
