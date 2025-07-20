16bd4571b071d018dc86559b4a64b5d50e95de76
Henrik Norlin <henrik.norl@gmail.com>
2025-07-20 18:23:53 +0200

    project_automation -> project_fiq

--- project_fiq/__manifest__.py
+++ project_fiq/__manifest__.py
@@ -1,15 +1,16 @@
-# Copyright 2025 Loym
+# Copyright 2025 FIQ
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "FIQ Project Automation",
+    "name": "FIQ Project",
     "summary": "Set alias & doc folder name",
     "author": "FIQ",
     "data": [],
     "depends": [
+        "project_internal_external",
         "project_unique_code",
     ],
     "license": "LGPL-3",
     "version": "18.0.3.0.19",
-    "website": "https://www.loym.com",
+    "website": "https://www.fiq.no",
 }
